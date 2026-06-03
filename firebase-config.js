a499d9a692542f134613b4f77f55fffa3b4c8cb9
prsthsbw9-dev <prsthsbw9@gmail.com>
2026-06-03 22:23:13 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDtrEyAnVMhCZWnSaUYut9LxNC17C3So1Y",
@@ -7,7 +8,9 @@ const firebaseConfig = {
   projectId: "parasha-site-links",
   storageBucket: "parasha-site-links.firebasestorage.app",
   messagingSenderId: "799400165769",
-  appId: "1:799400165769:web:1b5cf70eee80e772df6158"
+  appId: "1:799400165769:web:1b5cf70eee80e772df6158",
+  measurementId: "G-2MGXGC1KB8"
 };
 
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
